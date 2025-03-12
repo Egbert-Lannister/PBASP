a40b5e74272a4e9a4cbd693af916627e1a4c4ed5
Egbert-Lannister <22722030@bjtu.edu.cn>
2025-03-12 20:10:26 +0800

    精简接收功能

--- CloudServer_1.py
+++ CloudServer_1.py
@@ -10,44 +10,6 @@ from encryption import ProxyPseudorandom, UniversalReEncryption
 # from TailoredUniversalReEncryption.UniversalReEncryption_MultithreadingParallel import UniversalReEncryption
 # from UniversalReEncryption.Universal_ReEncryption_cpp_Acceleration import universal_reencryption
 
-def receive_data(sock):
-    """接收数据"""
-    try:
-        # 接收数据长度
-        data_length_bytes = sock.recv(4)
-        if not data_length_bytes:
-            return None
-        data_length = int.from_bytes(data_length_bytes, byteorder='big')
-
-        # 接收数据
-        received_data = b''
-        while len(received_data) < data_length:
-            chunk = sock.recv(data_length - len(received_data))
-            if not chunk:
-                break
-            received_data += chunk
-
-        # 反序列化数据
-        return pickle.loads(received_data)
-    except Exception as e:
-        print(f"接收数据时出错: {e}")
-        return None
-
-def send_to_server(data, server_address):
-    """发送数据到指定的服务器"""
-    try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            sock.connect(server_address)
-            # 序列化数据
-            serialized_data = pickle.dumps(data)
-            # 发送数据长度
-            sock.sendall(len(serialized_data).to_bytes(4, byteorder='big'))
-            # 发送数据
-            sock.sendall(serialized_data)
-        print(f"数据已成功发送到 {server_address}")
-    except Exception as e:
-        print(f"发送数据到 {server_address} 时出错: {e}")
-
 def main():
     HOST = 'localhost'
     cs1_PORT = 12345
