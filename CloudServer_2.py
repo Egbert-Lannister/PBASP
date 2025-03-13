b367bdc2ed29c2c32684a783ce126489b6d3657a
Egbert-Lannister <22722030@bjtu.edu.cn>
2025-03-13 14:56:36 +0800

    实现查询

--- CloudServer_2.py
+++ CloudServer_2.py
@@ -165,9 +165,23 @@ def main():
                 keyword_query_result = {}
                 position_query_result = {}
 
-                for encrypted_query_keyword in encrypted_query_keywords:
-                    keyword_query_result[encrypted_query_keyword] = re_encrypted_keyword_index_2_2nd[
-                        encrypted_query_keyword]
+                for qt_keyword in encrypted_query_keywords:
+                    # 查找时用 key 以 bytes 形式存储，故先转换为字符串
+                    init_token = qt_keyword.decode("utf-8")
+                    # 遍历加密关键字索引，寻找匹配项
+                    found = False
+                    for encrypted_keyword,  (capsule, encrypted_bitmap) in re_encrypted_keyword_index_2_2nd.items():
+                        # 获取重加密次数
+                        count = capsule.get("count", 0)
+                        # 对客户端原始令牌转换 count 次
+                        transformed_token = ProxyPseudorandom.transform_query_token(init_token, rk, count)
+                        # 与存储在 capsule 中的 tag 比较
+                        if transformed_token == capsule["tag"]:
+                            keyword_query_result[encrypted_keyword] = encrypted_bitmap
+                            found = True
+                            break
+                    if not found:
+                        keyword_query_result[init_token] = "NotFound"
 
                 for encrypted_query_prefix_code in encrypted_query_prefix_codes:
                     position_query_result[encrypted_query_prefix_code] = re_encrypted_position_index_2_2nd[
